c7b4eba9bc43d3beb2423b0a1b79ec5cab596228
Abraham George Smith <abe@abesmith.co.uk>
2022-01-21 11:39:21 +0100

    switched to use QMainWindow

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import sys
 from PyQt5 import QtWidgets 
 
-if __name__ == '__main__':
-   app = QtWidgets.QApplication(sys.argv)
-   widget = QtWidgets.QWidget()
-   label = QtWidgets.QLabel(widget)
-   label.setText("task timer")
-   label.move(10, 10)
-   widget.setGeometry(100, 100, 200, 60)
-   widget.setWindowTitle("Task timer")
-   widget.show()
-   sys.exit(app.exec_())
+class MainWindow(QtWidgets.QMainWindow):
+
+    def add_start_button(self):
+        start_button = QtWidgets.QPushButton('start', self)
+ 
+    def create_ui(self):
+       label = QtWidgets.QLabel(self)
+       label.setText("task timer")
+       label.move(10, 10)
+       self.setGeometry(100, 100, 200, 60)
+       self.setWindowTitle("Task timer")
+
+    def __init__(self):
+        super().__init__()
+        self.create_ui()
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec_())
